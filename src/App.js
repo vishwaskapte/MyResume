a9a7ed74b5947ded1cb2ea43b64ebb93a373b0a1
vishwaskapte <45562090+vishwaskapte@users.noreply.github.com>
2023-12-15 14:20:04 +0530

    gdgdgdg

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <div class="w3-content w3-margin-top">
         <div class="w3-row-padding">
-            <h2 class="w3-text-teal">VISHWAS VINAYAK KAPTE</h2>
+            <h2 class="w3-text-teal">A B C</h2>
             <LeftComponent />
             <RightComponent />
         </div>
--- src/Components/LeftComponent.js
+++ src/Components/LeftComponent.js
@@ -7,66 +7,64 @@ import StrengthComponent from './StrengthComponent'
 //Function Defination Area
 export default function LeftComponent() {
   return (
-        <div class="w3-third">
-            <div class="w3-white w3-text-grey w3-card-4">
-              <div class="w3-display-container">
-                
-                
-              </div>
-              <div class="w3-container">
-                <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Software Developer</p>
-                <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i> Pune, India </p>
-                <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>vishwaskapte@gmail.com</p>
-                <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>+91 9561153397</p>
-                <hr />
-      
-                <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Skills</b></p>
-                <SkillsComponent 
-                    skill = 'Adobe Photoshop'
-                    percent = '90%'
-                />
-                <SkillsComponent 
-                    skill = 'Photography'
-                    percent = '80%'
-                />
-                <SkillsComponent 
-                    skill = 'Illustrator'
-                    percent = '75%'
-                />
-                <SkillsComponent 
-                    skill = 'Media'
-                    percent = '50%'
-                />
-                <br />
-      
-                <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal" ></i>Languages</b></p>
-                <LanguagesComponent 
-                  name='English'
-                />
-                <LanguagesComponent 
-                  name='Spanish'
-                />
-                <LanguagesComponent 
-                  name='German'
-                />
-                <br />
-
-                <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Strengths</b></p>
-                <StrengthComponent 
-                    name = 'Problem Solving'
-                    description = 'Developed solutions for complex problems resulting in a 30% increase in efficiency'
-                />
-                <StrengthComponent 
-                    name = 'Teamwork'
-                    description = 'Collaborated with cross-functional teams resulting in successful project completion within deadline'
-                />
-                <StrengthComponent 
-                    name = 'Attention to Detail'
-                    description = 'Implemented rigorous testing methodologies resulting in 99% bug-free code'
-                />
-                <br />
-              </div>
-            </div><br />
+    <div class="w3-third">
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
         </div>
+        <div class="w3-container">
+          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Software Developer</p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i> Pune, India </p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>abc@gmail.com</p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>+91 985425635256</p>
+          <hr />
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Skills</b></p>
+          <SkillsComponent
+            skill='Adobe Photoshop'
+            percent='90%'
+          />
+          <SkillsComponent
+            skill='Photography'
+            percent='80%'
+          />
+          <SkillsComponent
+            skill='Illustrator'
+            percent='75%'
+          />
+          <SkillsComponent
+            skill='Media'
+            percent='50%'
+          />
+          <br />
+
+          <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal" ></i>Languages</b></p>
+          <LanguagesComponent
+            name='English'
+          />
+          <LanguagesComponent
+            name='Spanish'
+          />
+          <LanguagesComponent
+            name='German'
+          />
+          <br />
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Strengths</b></p>
+          <StrengthComponent
+            name='Problem Solving'
+            description='Developed solutions for complex problems resulting in a 30% increase in efficiency'
+          />
+          <StrengthComponent
+            name='Teamwork'
+            description='Collaborated with cross-functional teams resulting in successful project completion within deadline'
+          />
+          <StrengthComponent
+            name='Attention to Detail'
+            description='Implemented rigorous testing methodologies resulting in 99% bug-free code'
+          />
+          <br />
+        </div>
+      </div><br />
+    </div>
   )
 }
